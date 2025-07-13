47af7f12b7d6be6026e21c7027c1ec06d8acf4f5
binnichtaktiv <96953964+binnichtaktiv@users.noreply.github.com>
2025-07-13 14:50:38 +0200

    wrong version lmao

--- arcardePatcher.py
+++ arcardePatcher.py
@@ -1,58 +1,69 @@
 import os, random, zipfile, shutil, plistlib, subprocess
 
-tweakFolder = "/home/jonasb/Desktop/testtttt/tweaks"
+count = 0
+
+tweakFolder = ""
 if tweakFolder == "":
     input("please enter the folder path for all the bypass tweaks:\n")
     # ignore this if you hard coded your folder path in the "" above
 
-input = input("enter folder or iPA path:\n")
+uinput = input("enter folder or iPA path:\n")
+output = input("enter output path (leave it empty if you want it in the same path as input iPA/folder):\n")
+if output == None:
+    if uinput.endswith(".ipa"):
+        output = os.path.dirname(uinput)
+    else:
+        output = uinput
 
-if not input.endswith(".ipa"):
-    iPA = [os.path.join(input, file) for file in os.listdir(input) if os.path.isfile(os.path.join(input, file)) and file.endswith(".ipa")]
-    outputFolder = input
+if not uinput.endswith(".ipa"):
+    iPA = [os.path.join(uinput, file) for file in os.listdir(uinput) if os.path.isfile(os.path.join(uinput, file)) and file.endswith(".ipa")]
     print("[*] folder with iPA(s) detected")
 else:
-    iPA = input
-    outputFolder = os.path.dirname(input)
+    iPA = [uinput]
     print("[*] single iPA detected")
 
-print("[*] extracting iPA")
-print(iPA)
-iPATmp = os.mkdir(os.path.join(outputFolder, str(random.randint(100000, 999999))))
-print(iPATmp)
-with zipfile.ZipFile(iPA, "r") as zip:
-    zip.extractall(iPATmp)
+for entry in iPA:
+    print("[*] extracting iPA")
+    iPATmp = os.path.join(output, str(random.randint(100000, 999999)))
+    os.mkdir(iPATmp)
 
-Payload = os.path.join(iPATmp, "Payload")
+    with zipfile.ZipFile(entry, "r") as zip:
+        zip.extractall(iPATmp)
 
-appDir = list(Payload.glob("*.app"))
-if not appDir:
+    Payload = os.path.join(iPATmp, "Payload")
+    appDir = [os.path.join(Payload, f) for f in os.listdir(Payload) if f.endswith(".app") and os.path.isdir(os.path.join(Payload, f))]
+    if not appDir:
+        shutil.rmtree(iPATmp)
+        raise Exception("[!] no .app folder found")
+
+    print("[*] modifing Info.plist")
+
+    plist = os.path.join(appDir[0], "Info.plist")
+    if not plist:
+        shutil.rmtree(iPATmp)
+        raise Exception("[!] no Info.plist found")
+    
+    with open(plist, "rb") as fp:
+        pl = plistlib.load(fp)
+
+    pl["NSApplicationRequiresArcade"] = False
+
+    with open(plist, "wb") as fp:
+        plistlib.dump(pl, fp)
+
+    print("[*] injecting tweaks")
+
+    patchedOutput = os.path.join(output, "patched iPas")
+    os.makedirs(patchedOutput, exist_ok=True)
+
+    inject = ['"' + os.path.join(tweakFolder, file) + '"' for file in os.listdir(tweakFolder)]
+
+    cyanCmd = f"cyan -o '{os.path.join(patchedOutput,os.path.basename(entry[:-4] + "_patched.ipa"))}' -i '{appDir[0]}' -f {' '.join(inject)} -c 9 -gqw"
+
+    subprocess.call(cyanCmd, shell=True)
+
     shutil.rmtree(iPATmp)
-    raise Exception("[!] no .app folder found")
 
-print("[*] modifing Info.plist")
-
-plist = os.path.join(appDir, "Info.plist")
-if not plist:
-    shutil.rmtree(iPATmp)
-    raise Exception("[!] no Info.plist found")
-   
-with open(plist, "rb") as fp:
-    pl = plistlib.load(fp)
-
-pl["NSApplicationRequiresArcade"] = False
-
-with open(plist, "wb") as fp:
-    plistlib.dump(pl, fp)
-
-print("[*] injecting tweaks")
-patchedOutput = os.makedirs(os.path.join(outputFolder, "patched iPas"), exist_ok=True)
-injectThis = [file for file in tweakFolder]
-inject = ' '.join(['"' + item + '"' for item in injectThis])
-
-cyanCmd = f"cyan -o {os.path.join(patchedOutput,os.path.basename(iPA))} -i {appDir} -f {inject} -c 9 -gqw"
-
-subprocess.call(cyanCmd)
-
-print("[*] deleting temp directory")
-shutil.rmtree(iPATmp)
+    if len(iPA) > 1:
+        count+=1
+        print(f"[*] app {count} of {len(iPA)} done\n")
